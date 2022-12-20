9072067ab260af1e4b0d45a3e6c2c70aca3f3d0b
botirmasharipov <masbotirjon963@gmail.com>
2022-12-20 13:00:37 -0500

    add state to page

--- public/react/components/Page.js
+++ public/react/components/Page.js
@@ -1,9 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react'
+import apiURL from '../api'
 
 export const Page = (props) => {
+  const [people, setPeople] = useState("")
+
+  const handleClick = async () => {
+    // single article page
+    const res = await fetch(`${apiURL}/wiki/${props.page.slug}`)
+    const data = await res.json()
+    console.log(data)
+    setPeople(data)
+  }
 
   return <>
-    <h3>{props.page.title}</h3>
+    <h3 onClick={handleClick}>{props.page.title}</h3>
+    <p>{people ? people.author.name : ""}</p>
   </>
-} 
-	
+}
